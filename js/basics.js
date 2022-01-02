a5ab205b8429dfcac17d1f175f6b718caa172509
me6edi <exited17@gmail.com>
2022-01-02 22:26:45 +0600

    Array

--- js/basics.js
+++ js/basics.js
@@ -113,8 +113,8 @@ This is a comment multi line
 
 // var myClass = 200
 // console.log(myClass)
-(function(){
-    'use strict';
+// (function(){
+//     'use strict';
     // if(true == false){
     //     console.log('we well see this')
     // }
@@ -133,22 +133,113 @@ This is a comment multi line
 
 
 
-    //switch
-    var switcher = 'one';
-    switch(switcher){
-        case 'one':
-            console.log('one');
-            break;
-        case 'two':
-            console.log('two')
-            break;
-    }
-// for loop 
-var anArray = ['item1','item2','item3'];
-for (var x = 0; x < anArray.length; x++){
-    console.log(anArray[x]);
-}
-//for in loop 
+//     //switch
+//     var switcher = 'one';
+//     switch(switcher){
+//         case 'one':
+//             console.log('one');
+//             break;
+//         case 'two':
+//             console.log('two')
+//             break;
+//     }
+// // for loop 
+// var anArray = ['item1','item2','item3'];
+// for (var x = 0; x < anArray.length; x++){
+//     console.log(anArray[x]);
+// }
+// //for in loop 
+//     var anObject = {
+//         prop1 : 'property1',
+//         prop1 : 'property2',
+//         prop3 : 'property3'
+//     };
+//     for (var prop in anObject){
+//         if(anObject.hasOwnProperty(prop)){
+//             console.log(anObject[prop]);
+//         }
+//     }
+//     //while loop
+//     var limit = 0;
+//     while(limit < 5){
+//         if(limit==3){
+//             break;
+//         }
+//         console.log(limit+=1)
+//     }
+    // //do while
+    // var exe = true;
+    // do{
+    //     console.log('once');
+    //     break;
+    // }while(exe)
 
-}());
+    // //setTimeout
+    // function log(msg){
+    //     console.log(msg)
+    // }
+
+    // setTimeout(function(){
+    //     log('hello!!');
+    // },2000);
+
+    // //setInterval
+    // function inter(){
+    //     console.log('this is interval');
+    //     counter+=1;
+
+    //     if(counter === 5){
+    //         clearInterval(intervalID)
+    //     }
+    // }
+    // var intervalID = setInterval(inter, 1000);
+    // var counter = 0;
+
+    //String
+
+// }());
+
+(function(){
+    'use strict';
+
+    // var testString = 'test string';
+
+    // console.log(testString.length);
+    // console.log(testString.split(' '));
+    // console.log(testString.indexOf('t', 1));
+    // console.log(testString.lastIndexOf('t'));
+
+
+    // console.log(testString.toUpperCase());
+    // console.log(testString.toLowerCase());
+
+    // console.log(testString.substring(3))
+    // console.log(testString.substring(0,4))
+
+    // console.log(testString.slice(5))
+    // console.log(testString.slice(-2))
+
+    // console.log('    this  '.trim());
+
+    var array1 = ['one', 'two', 'three','four'];
+    console.log(array1.length);
+
+    array1.push('five');
+    array1.unshift('zero');
+    array1.pop();
+    array1.splice(0,2);
+    array1.splice(0,4,'a','b','c','d');
+    array1.reverse();
+    array1.sort();
+    console.log(array1);
+
+    var array2 = [20,30,100,10];
+    array2.sort();
+
+    console.log(array2);
+
+
+
+
+}())
 
