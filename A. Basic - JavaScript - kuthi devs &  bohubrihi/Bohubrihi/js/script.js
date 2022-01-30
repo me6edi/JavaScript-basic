a7566505e2184efdbbdabf7593883fe00184abd1
me6edi <exited17@gmail.com>
2022-01-30 22:15:20 +0600

    Operators

--- A. Basic - JavaScript - kuthi devs &  bohubrihi/Bohubrihi/js/script.js
+++ A. Basic - JavaScript - kuthi devs &  bohubrihi/Bohubrihi/js/script.js
@@ -1,3 +1,118 @@
-console.log("I am first Line")
-console.log("I am second Line")
-document.write("JS file is connected")
+// singleline Comment
+// console.log("I am first Line");
+
+
+// Multiline Comment
+/*console.log ("I am second Line");
+document.write('JS file is connected');*/
+
+
+// document.write(67);
+
+
+//User Input in JS
+/*var x;
+x = prompt("Enter your name ");
+document.write(x)
+console.log(x)*/
+
+
+// JavaScritpt Variables
+/*var a;
+let b;
+a = 45;
+console.log(a);
+a = 35;
+b = "Mehedi";
+console.log(a);
+console.log(b);
+a = "Mehedi Amin"
+console.log(a)*/
+/*var a = 20;
+var b = a;
+console.log(a);
+console.log(b);
+a = 30;
+console.log(a);
+console.log(b);*/
+
+/*var a = 20;
+var b = 10;
+var c = a + b;
+console.log(c)
+
+var MyName, myName;
+MyName = "abcd";
+myName = "efgh";
+console.log(MyName);
+console.log(myName);
+
+var a = 20;
+console.log(a);
+var a;
+console.log(a)*/
+
+// javaScript Constants
+// const a;
+// a = 10;
+// var b;
+// b = 20
+// console.log(b)
+
+// kywords
+
+// var,const,do,else
+ 
+// Operator
+// Arithmetic Operators
+// 2 + 4
+//  + - * / %
+
+//Increment a++ / ++a
+// a++; a = a+1;
+// Increment a++ / ++a
+// var a = 5;
+// var b = 7;
+
+// var c, d;
+// c = a++;
+// d = ++b;
+// console.log(c);
+// console.log(a);
+// console.log(d);
+// console.log(b);
+
+// //decrement  e++ / ++f
+// console.log("Decrement  e-- / --f")
+
+// var e = 5;
+// var f = 7;
+
+// var g, h;
+// g = e--;
+// h = --f;
+// console.log(g);
+// console.log(e);
+// console.log(h);
+// console.log(f);
+
+//BODMAS
+// a = 3 - 5 * 5 / 7 + 4**4;
+// console.log(a);
+
+//String Operator
+// NaN = > Not a Number
+
+
+//Comparison Operators
+
+// 10<20,30>10,1<=10,1<=-3,1 <= 2
+// var a = (5>4);
+// console.log(a)
+
+
+//Logical Operators
+// &&,||,!
+
+//Conditional Operators
+// var a = (10 < 18) ? "Hello":"World";
