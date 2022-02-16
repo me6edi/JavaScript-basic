6e072681b526dc044d1d470ffb25966e4058fa5f
me6edi <exited17@gmail.com>
2022-02-16 19:34:10 +0600

    Switch statement

--- A. Basic - JavaScript - kuthi devs &  bohubrihi/Bohubrihi/js/script.js
+++ A. Basic - JavaScript - kuthi devs &  bohubrihi/Bohubrihi/js/script.js
@@ -271,3 +271,64 @@ console.log(a)*/
 // else{
 //     console.log("Invalid Input!")
 // }
+
+//Find the largest number
+// var n1 = prompt("First number: ");
+// var n2 = prompt("Second number: ");
+// var n3 = prompt("Third number: ");
+// n1 = parseInt(n1); // 10
+// n2 = parseInt(n2); // 20
+// n3 = parseInt(n3); // 30
+
+// if ( n1 >= n2 && n1 >= n3) {  // 50 >= 30 && 50 >= 20
+//     console.log(n1 + " is the largest number!");
+// }
+// else if(n2 >= n1 && n1 >= n3){ // 30 >= 50 && 50 >= 20
+//     console.log(n2 + " is the largest number!");
+// }
+// else{
+//     console.log(n3 + " is the largest number!");
+// }
+
+// if (n1 >= n2 ){ // 10 >= 20
+//     if (n1 >= n3){ //10 >= 30
+//         console.log(n1 + " n1 is the largest number!")
+//     }
+//     else{
+//         console.log(n3 + " n3 is the largest number!")
+//     }
+// }else{
+//     if(n2 >= n3){ // 20 >= 30
+//         console.log(n2 + " n2 is the largest number!")
+//     }
+//     else{
+//         console.log(n3 + " n3 is the largest number!")
+//     }
+// }
+
+
+// Switch
+
+// console.log("Select an option:\na. Option 1 \nb. Option 2 \nc. Option 3 ");
+
+// choice = prompt();
+
+// var text;
+
+// switch(choice){
+//     case "a" : 
+//         text = "Option 1 Selected!"
+//         break;
+
+//     case "b" : 
+//         text = "Option 2 Selected!"
+//         break;
+
+//     case "c" : 
+//         text = "Option 3 Selected!"
+//         break;
+
+//     default:
+//         text = "No option is selected!";
+//         break;
+// }
